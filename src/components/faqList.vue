b6e5b8f4b20774fc6c94e3e6b1382e7c0160da7d
Nazafat7 <nazafat007@gmail.com>
2023-05-13 13:52:28 +0530

    minor changes

--- src/components/faqList.vue
+++ src/components/faqList.vue
@@ -68,7 +68,7 @@
               <div class="modal-dialog modal-lg">
                 <div class="modal-content">
                   <div class="modal-header">
-                    <h5 class="modal-title" id="edit_faqModelLabel">Add FAQ</h5>
+                    <h5 class="modal-title" id="edit_faqModelLabel">Edit FAQ</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
                   <div class="modal-body">
